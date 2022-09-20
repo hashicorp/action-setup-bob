8f043706c279919b825490a8ca1188a6d50a5c91
Michele Degges <mdeggies@gmail.com>
2022-09-20 16:44:39 -0700

    Bump bob to v0.0.30

--- bob.js
+++ bob.js
@@ -5,7 +5,7 @@ const githubRelease = require('./github-release');
 const executableName = 'bob';
 const gitHubRepositoryOwner = 'hashicorp';
 const gitHubRepositoryRepo = 'bob';
-const latestVersion = '0.0.29';
+const latestVersion = '0.0.30';
 
 async function downloadReleaseAsset(client, releaseAsset, directory) {
   return await githubRelease.downloadAsset(client, gitHubRepositoryOwner, gitHubRepositoryRepo, releaseAsset, directory);
